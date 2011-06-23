4ca160edb598ff66ffe804cb3a71e7580d5cdbd4
Jester Durden <lewandel@gmail.com>
2011-06-24 00:40:46 +0200

    Done with sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to @user      
     else
       @title = "Sign up"
-      render 'new'
+      @user.password = ""
+      @user.password_confirmation = ""
+      render 'new'      
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,11 +8,11 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  
+
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
-  
+
   RSpec.configure do |config|
     # == Mock Framework
     #
@@ -22,14 +22,17 @@ Spork.prefork do
     # config.mock_with :flexmock
     # config.mock_with :rr
     config.mock_with :rspec
-  
+
     # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  
+
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, remove the following line or assign false
     # instead of true.
     config.use_transactional_fixtures = true
+    def test_sign_in(user)
+      controller.sign_in(user)
+    end
   end
 
 end
@@ -67,7 +70,4 @@ end
 #
 # These instructions should self-destruct in 10 seconds.  If they don't, feel
 # free to delete them.
-
-
-
 
